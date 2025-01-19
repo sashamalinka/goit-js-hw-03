6a57a552b266417d937eae1cb5a9e2cb5931da6a
Sasha <malinka15117kk@gmail.com>
2025-01-19 19:00:30 +0200

    hm3

--- js/task-2.js
+++ js/task-2.js
@@ -10,8 +10,9 @@
 
 function makeArray(firstArray, secondArray, maxLength) {
     const array = firstArray.concat(secondArray);
-    if (array.length > maxLength);
-    return array.slice(0, maxLength);
+    if (array.length > maxLength) {
+        return array.slice(0, maxLength);
+    }
     return array;
 }
 
